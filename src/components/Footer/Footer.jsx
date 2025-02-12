8eca2cd1110f45564f9c06bdad60d988b9ad1728
gedion <legessegedion@gmail.com>
2025-02-11 20:46:07 -0500

    making header & footer Resposive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,68 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-white ">
+      <div className="container">
+        <div className="row ">
+          <div className="col-12 d-flex  gap-3">
+            <FacebookOutlinedIcon />
+            <InstagramIcon />
+            <YouTubeIcon />
+          </div>
+        </div>
+
+        <div className="row ">
+          <div className="col-md-3 col-sm-6 ">
+            <ul className="list-unstyled">
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6 ">
+            <ul className="list-unstyled">
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6 ">
+            <ul className="list-unstyled">
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6 ">
+            <ul className="list-unstyled">
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row col-12 service_code">
+          <p>Service Code</p>
+        </div>
+
+        <div className="row col-12 ">
+          <p>&copy; 1997-2024 Netflix, Inc.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
+
+/*import React from "react";
 import "./footer.css";
 import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 import InstagramIcon from "@mui/icons-material/Instagram";
@@ -53,4 +117,4 @@ const Footer = () => {
   );
 };
 
-export default Footer;
+export default Footer;*/
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,66 @@
 import React from "react";
+
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+import HeaderLogo from "../../assets/logo/netflix-4.svg";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SearchIcon from "@mui/icons-material/Search";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+const Header = () => {
+  return (
+    <Navbar
+      variant="dark"
+      expand="lg"
+      className="bg-dark transparent text-white sticky-top"
+    >
+      <Container fluid>
+        <Navbar.Brand href="#">
+          <img src={HeaderLogo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1 ">Home</Nav.Link>
+            <Nav.Link href="#action2">TVShow</Nav.Link>
+            <Nav.Link href="#action2">Movies</Nav.Link>
+            <Nav.Link href="#action2">Latest</Nav.Link>
+            <Nav.Link href="#action2">MyList</Nav.Link>
+            <Nav.Link href="#action2">Browse by Languages</Nav.Link>
+
+            <Nav.Link href="#" disabled></Nav.Link>
+          </Nav>
+          <Form className="d-flex gap-3 text-white">
+            <Nav.Link href="#" disabled>
+              <SearchIcon />
+            </Nav.Link>
+            <Nav.Link href="#" disabled></Nav.Link>
+            <Nav.Link href="#action2">
+              <NotificationsNoneIcon />
+            </Nav.Link>
+            <Nav.Link href="#" disabled></Nav.Link>
+            <Nav.Link href="#" disabled>
+              <AccountBoxIcon />
+            </Nav.Link>
+
+            <ArrowDropDownIcon />
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
+
+/*import React from "react";
 import "./header.css";
 import HeaderLogo from "../../assets/logo/netflix-4.svg";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
@@ -44,4 +106,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default Header;*/
